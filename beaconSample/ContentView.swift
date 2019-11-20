5ed86aa02a5b22d0c418cb238695b1efda0e7c8e
Ryu Tanabe <r-tanabe@media-inc.co.jp>
2019-11-20 16:34:53 +0900

    Initial Commit

---
+++ beaconSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beaconSample
+//
+//  Created by tanabe on 2019/11/20.
+//  Copyright © 2019 Ryu Tanabe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
